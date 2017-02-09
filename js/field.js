1fe6ebd3745db165d05c50840fb43bfe6a0fc1e4
Aliot <aliot@tut.by>
2017-02-09 09:09:31 +0300

    added classses

--- js/field.js
+++ js/field.js
@@ -1,9 +1,9 @@
-(function() {
+(function () {
 
     var maxCol = 16;
     var maxRow = 16;
     var direction = 'right';
-    var snake = [73,72,71,5];
+    var snake = [73, 72, 71, 5];
 
 
     function createField(rowC, colC) {
@@ -44,9 +44,7 @@
         var idColRand = randomInteger(0, maxCol);
         var id = 'row' + idRowRand + 'col' + idColRand;
         var elem = document.getElementById(id);
-        if(!elem.hasAttribute('style')) {
-            elem.style.background = "red";
-        }
+        elem.classList.add('food');
     }
 
     var a = setInterval(createFood, 500);
@@ -55,13 +53,14 @@
         //var sn = document.getElementById("row1col1");
         var len = snake.length;
         console.log(len);
-        for (var i = 1; i < len+1; i++) {
+        for (var i = 1; i < len ; i++) {
             var id = 'row' + 1 + 'col' + i;
             var elem = document.getElementById(id);
-            elem.style.background = "black";
+            elem.classList.add('snake');
         }
     }
-    //generateSnake();
+
+    generateSnake();
 
     var x = 1;
     var y = 1;
@@ -71,19 +70,18 @@
             var id = 'row' + y + 'col' + x;
             console.log(id);
             var elem = document.getElementById(id);
-            elem.style.background = "black";
+            elem.classList.add('snake');
         }
-
-            if(x > 3 && x < maxCol + 1){
-                var idnext = 'row' + y + 'col' + (x - 3);
-                console.log(idnext);
-                var elemnext = document.getElementById(idnext);
-                elemnext.style.background = "ghostwhite";
-            } else if (x > maxCol) {
-                var idpost = 'row' + y + 'col' + (x - 3);
-                var elempost = document.getElementById(idpost);
-                elempost.style.background = "ghostwhite";
-            }
+        if (x > 3 && x < maxCol + 1) {
+            var idnext = 'row' + y + 'col' + (x - 3);
+            console.log(idnext);
+            var elemnext = document.getElementById(idnext);
+            elemnext.classList.remove('snake');
+        } else if (x > maxCol) {
+            var idpost = 'row' + y + 'col' + (x - 3);
+            var elempost = document.getElementById(idpost);
+            elempost.classList.remove('snake');
+        }
         x++;
         if (x < maxCol + 4) {
             setTimeout(runSnake, 100);
@@ -99,20 +97,21 @@
             }
         }
     }
+
     runSnake();
 
-   /*
+    /*
 
-    function runSnake(i) {
-        if (i >= 0 && i < (xmax * ymax)) {
-            var tr = Math.floor(i / xmax);
-            var td = i % xmax;
-            var oTd = jQuery('#tbl').find('tr').eq(tr).find('td').eq(td);
-            return oTd;
-        } else {
-            return false;
-        }
-    }*/
+     function runSnake(i) {
+     if (i >= 0 && i < (xmax * ymax)) {
+     var tr = Math.floor(i / xmax);
+     var td = i % xmax;
+     var oTd = jQuery('#tbl').find('tr').eq(tr).find('td').eq(td);
+     return oTd;
+     } else {
+     return false;
+     }
+     }*/
 })();
 
 
